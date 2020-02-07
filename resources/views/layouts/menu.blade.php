a5d1dd92d837b1ee1a707777e97f2ed4d7cc55c6
sonutss <sonumalik@teastallstudio.com>
2020-02-07 12:29:10 +0530

    change

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -74,16 +74,16 @@
         <div class="br-header-left">
             <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href="#"><i class="icon ion-navicon-round"></i></a></div>
             <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href="#"><i class="icon ion-navicon-round"></i></a></div>
-            <div class="input-group hidden-xs-down wd-170 transition">
+            <!-- <div class="input-group hidden-xs-down wd-170 transition">
                 <input id="searchbox" type="text" class="form-control" placeholder="Search">
                 <span class="input-group-btn">
             <button class="btn btn-secondary" type="button"><i class="fas fa-search"></i></button>
           </span>
-            </div>
+            </div> -->
         </div>
         <div class="br-header-right">
             <nav class="nav">
-                <div class="dropdown">
+                <!-- <div class="dropdown">
                     <a href="#" class="nav-link pd-x-7 pos-relative" data-toggle="dropdown">
                         <i class="icon ion-ios-email-outline tx-24"></i>
                         <span class="square-8 bg-danger pos-absolute t-15 r-0 rounded-circle"></span>
@@ -147,8 +147,8 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="dropdown">
+                </div> -->
+                <!-- <div class="dropdown">
                     <a href="#" class="nav-link pd-x-7 pos-relative" data-toggle="dropdown">
                         <i class="icon ion-ios-bell-outline tx-24"></i>
                         <span class="square-8 bg-danger pos-absolute t-15 r-5 rounded-circle"></span>
@@ -200,9 +200,9 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <div class="dropdown">
-                    <a href="#" class="nav-link nav-link-profile" data-toggle="dropdown">
+                    <!-- <a href="#" class="nav-link nav-link-profile" data-toggle="dropdown">
                         <span class="logged-name hidden-md-down">Katherine</span>
                         <img src="{{ url('public/frontend/img/img1.jpg') }}" class="wd-32 rounded-circle" alt="">
                         <span class="square-10 bg-success"></span>
@@ -218,14 +218,14 @@
                             <span class="profile-earning-label">Earnings After Taxes</span>
                             <h3 class="profile-earning-amount">$13,230 <i class="icon ion-ios-arrow-thin-up tx-success"></i></h3>
                             <span class="profile-earning-text">Based on list price.</span>
-                        </div>
-                        <hr>
+                        </div> -->
+                        <!-- <hr> -->
                         <ul class="list-unstyled user-profile-nav">
-                            <li><a href="#"><i class="icon ion-ios-person"></i> Edit Profile</a></li>
+                            <!-- <li><a href="#"><i class="icon ion-ios-person"></i> Edit Profile</a></li>
                             <li><a href="#"><i class="icon ion-ios-gear"></i> Settings</a></li>
                             <li><a href="#"><i class="icon ion-ios-download"></i> Downloads</a></li>
                             <li><a href="#"><i class="icon ion-ios-star"></i> Favorites</a></li>
-                            <li><a href="#"><i class="icon ion-ios-folder"></i> Collections</a></li>
+                            <li><a href="#"><i class="icon ion-ios-folder"></i> Collections</a></li> -->
                             <li><a href="{{ url('logout') }}"><i class="icon ion-power"></i> Sign Out</a></li>
                         </ul>
                     </div>

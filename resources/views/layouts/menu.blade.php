7c298248f0da1e6ed9ac102b441e447153e554d9
sonutss <sonumalik@teastallstudio.com>
2020-02-10 11:08:19 +0530

    changes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -74,12 +74,12 @@
         <div class="br-header-left">
             <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href="#"><i class="icon ion-navicon-round"></i></a></div>
             <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href="#"><i class="icon ion-navicon-round"></i></a></div>
-            <!-- <div class="input-group hidden-xs-down wd-170 transition">
+            <div class="input-group hidden-xs-down wd-170 transition">
                 <input id="searchbox" type="text" class="form-control" placeholder="Search">
                 <span class="input-group-btn">
             <button class="btn btn-secondary" type="button"><i class="fas fa-search"></i></button>
           </span>
-            </div> -->
+            </div>
         </div>
         <div class="br-header-right">
             <nav class="nav">
